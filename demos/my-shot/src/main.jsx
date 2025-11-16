b30b0494a950964e58b7e162dc98bdd020a4a85e
Partyk1d24 <jackiegleason@gmail.com>
2025-11-16 15:47:07 -0500

    Trying to push up

--- demos/my-shot/src/main.jsx
+++ demos/my-shot/src/main.jsx
@@ -1,18 +1,21 @@
 import { StrictMode } from 'react'
-import { StyledEngineProvider } from '@mui/material/styles';
+import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import { routes } from './routes'
+import theme from './theme'
 import './index.css'
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <StyledEngineProvider enableCssLayer>
-          <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
-          <RouterProvider router={router} />
-      </StyledEngineProvider>
+    <StyledEngineProvider enableCssLayer>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </StyledEngineProvider>
   </StrictMode>,
 )
